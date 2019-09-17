3eb16f0079945305c7e35e21151b0a91218254dc
mihaitoropu <toropumihai@gmail.com>
2019-09-17 12:02:04 +0300

    Base Controller Class

--- app/libraries/Controller.php
+++ app/libraries/Controller.php
@@ -1,7 +1,27 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: torop
- * Date: 9/16/2019
- * Time: 2:53 PM
- */
+	/*
+	 * Base controller
+	 * Loads the models and views
+	 */
+
+	class Controller {
+		// Load model
+		public function model($model)
+		{
+			require_once '../models/' . $model . '.php';
+
+			return new $model();
+		}
+
+		// Load view
+		public function view($view, $data = [])
+		{
+			// Check for view file
+			if (file_exists('../app/views/' . $view . '.php')) {
+				require_once '../app/views/' . $view . '.php';
+			} else {
+				// View dose not exist
+				die('View dose not exist');
+			}
+		}
+	}
