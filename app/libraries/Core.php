a5695ea6d1975959662f9b4fa7c6a642be59a65a
mihaitoropu96 <54101673+mihaitoropu96@users.noreply.github.com>
2019-09-17 10:46:06 +0300

    Update Core.php

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -15,11 +15,34 @@
 
  	public function __construct()
 	{
-		$this->getUrl();
+		//print_r($this->getUrl());
+		$url = $this->getUrl();
+
+		// Look in controllers for first value
+		if(file_exists('../app/controllers/' . ucwords($url[0]) . 'php')){
+			// If exists, set as controller
+			$this->currentController = ucwords($url[0]);
+
+			unset($url[0]);
+		}
+
+		// Require the controller
+		require_once '../app/controllers/' . $this->currentController . '.php';
+
+		// Instantiate controller class
+		$this->currentController = new $this->currentController;
+
+
 	}
 
-	 public function getUrl() {
- 		echo $_GET['url'];
+	 public function getUrl()
+	 {
+ 		if (isset($_GET['url'])) {
+ 			$url = rtrim($_GET['url'], '/');
+ 			$url = filter_var($url, FILTER_SANITIZE_URL);
+ 			$url = explode('/', $url);
+ 			return $url;
+		}
 	}
  }
 
