b455ec10db16bb6873a6298582f78772f7484014
mihaitoropu <toropumihai@gmail.com>
2019-09-20 15:04:56 +0300

    Edit + Delete POST

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -1,82 +1,160 @@
 <?php
 
-	class Posts extends Controller {
+class Posts extends Controller
+{
 
-		public function __construct()
-		{
-			if (!isLoggedIn()) {
-				redirect('users/login');
-			}
-
-			$this->postModel = $this->model('Post');
-			$this->userModel = $this->model('User');
+	public function __construct()
+	{
+		if (!isLoggedIn()) {
+			redirect('users/login');
 		}
 
-		public function index() {
-			// Get posts
-			$posts = $this->postModel->getPosts();
+		$this->postModel = $this->model('Post');
+		$this->userModel = $this->model('User');
+	}
+
+	public function index()
+	{
+		// Get posts
+		$posts = $this->postModel->getPosts();
+
+		$data = [
+			'posts' => $posts,
+		];
+
+		$this->view('posts/index', $data);
+	}
+
+	public function add()
+	{
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			// Sanitize the post
+			$_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
 			$data = [
-				'posts' => $posts,
+				'title' => trim($_POST['title']),
+				'body' => trim($_POST['body']),
+				'user_id' => $_SESSION['user_id'],
+				'title_err' => '',
+				'body_err' => '',
 			];
 
-			$this->view('posts/index', $data);
-		}
+			// Validate data
+			if (empty($data['title'])) {
+				$data['title_err'] = 'Please enter title';
+			}
+			if (empty($data['body'])) {
+				$data['body_err'] = 'Please body text';
+			}
 
-		public function add() {
-			if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-				// Sanitize the post
-				$_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
-
-				$data = [
-					'title' => trim($_POST['title']),
-					'body' => trim($_POST['body']),
-					'user_id' => $_SESSION['user_id'],
-					'title_err' => '',
-					'body_err' => '',
-				];
-
-				// Validate data
-				if (empty($data['title'])) {
-					$data['title_err'] = 'Please enter title';
-				}
-				if (empty($data['body'])) {
-					$data['body_err'] = 'Please body text';
-				}
-
-				// Make sure no errors
-				if (empty($data['title_err']) && empty($data['body_err'])) {
-					// Validated
-					if ($this->postModel->addPost($data)) {
-						flash('post_message', 'Post added');
-						redirect('posts');
-					} else {
-						die('Something went wrong');
-					}
+			// Make sure no errors
+			if (empty($data['title_err']) && empty($data['body_err'])) {
+				// Validated
+				if ($this->postModel->addPost($data)) {
+					flash('post_message', 'Post added');
+					redirect('posts');
 				} else {
-					// Load the view with errors
-					$this->view ('posts/add', $data);
+					die('Something went wrong');
 				}
 			} else {
-				$data = [
-					'title' => '',
-					'body' => '',
-				];
-
+				// Load the view with errors
 				$this->view('posts/add', $data);
 			}
-		}
-
-		public function show($id)
-		{
-			$post = $this->postModel->getPostById($id);
-			$user = $this->userModel->getUserById($post->user_id);
-
+		} else {
 			$data = [
-				'post' => $post,
-				'user' => $user,
+				'title' => '',
+				'body' => '',
 			];
 
-			$this->view('posts/show', $data);
+			$this->view('posts/add', $data);
 		}
 	}
+
+	public function show($id)
+	{
+		$post = $this->postModel->getPostById($id);
+		$user = $this->userModel->getUserById($post->user_id);
+
+		$data = [
+			'post' => $post,
+			'user' => $user,
+		];
+
+		$this->view('posts/show', $data);
+	}
+
+	public function edit($id)
+	{
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			// Sanitize the post
+			$_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+			$data = [
+				'id' => $id,
+				'title' => trim($_POST['title']),
+				'body' => trim($_POST['body']),
+				'user_id' => $_SESSION['user_id'],
+				'title_err' => '',
+				'body_err' => '',
+			];
+
+			// Validate data
+			if (empty($data['title'])) {
+				$data['title_err'] = 'Please enter title';
+			}
+			if (empty($data['body'])) {
+				$data['body_err'] = 'Please body text';
+			}
+
+			// Make sure no errors
+			if (empty($data['title_err']) && empty($data['body_err'])) {
+				// Validated
+				if ($this->postModel->updatePost($data)) {
+					flash('post_message', 'Post updated');
+					redirect('posts');
+				} else {
+					die('Something went wrong');
+				}
+			} else {
+				// Load the view with errors
+				$this->view('posts/edit', $data);
+			}
+		} else {
+			$post = $this->postModel->getPostById($id);
+
+			// Check for owner
+			if ($post->user_id != $_SESSION['user_id']) {
+				redirect('posts');
+			}
+			$data = [
+				'id' => $id,
+				'title' => $post->title,
+				'body' => $post->body,
+			];
+
+			$this->view('posts/edit', $data);
+		}
+	}
+
+	public function delete($id)
+	{
+		if ($_SERVER['REQUEST_METHOD'] == "POST") {
+			$post = $this->postModel->getPostById($id);
+
+			// Check for owner
+			if ($post->user_id != $_SESSION['user_id']) {
+				redirect('posts');
+			}
+
+			if ($this->postModel->deletePost($id)) {
+				flash('post_message', 'Post Removed');
+				redirect('posts');
+			} else {
+				die('Something went wrong!');
+			}
+		} else {
+			redirect('posts');
+		}
+	}
+}
+
--- app/models/Post.php
+++ app/models/Post.php
@@ -52,4 +52,35 @@ class Post
 		return $row;
 	}
 
+	public function updatePost($data)
+	{
+		$this->db->query('UPDATE post SET title = :title, body = :body WHERE id = :id');
+		// Bind values
+		$this->db->bind(':id', $data['id']);
+		$this->db->bind(':title', $data['title']);
+		$this->db->bind(':body', $data['body']);
+
+		// Execute
+		if ($this->db->execute()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public function deletePost($id)
+	{
+		$this->db->query('DELETE FROM post WHERE id = :id');
+
+		// Bind values
+		$this->db->bind(':id', $id);
+
+		// Execute
+		if ($this->db->execute()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 }
