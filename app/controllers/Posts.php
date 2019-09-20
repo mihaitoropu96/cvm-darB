2d1283afd1533ceedb6c41ecd98db5b0611a6056
mihaitoropu <toropumihai@gmail.com>
2019-09-20 11:54:51 +0300

    Restrict Access to unlogged user to view posts

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -1,6 +1,14 @@
 <?php
 
 	class Posts extends Controller {
+
+		public function __construct()
+		{
+			if (!isLoggedIn()) {
+				redirect('users/login');
+			}
+		}
+
 		public function index() {
 			$data = [];
 
--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -171,7 +171,7 @@ class Users extends Controller
 		$_SESSION['user_id'] = $user->id;
 		$_SESSION['user_email'] = $user->email;
 		$_SESSION['user_name'] = $user->name;
-		redirect('pages/index');
+		redirect('posts');
 	}
 
 
@@ -183,14 +183,5 @@ class Users extends Controller
 
 		session_destroy();
 		redirect('users/login');
-	}
-
-	public function isLoggedIn()
-	{
-		if (isset($_SESSION['user_id'])) {
-			return true;
-		} else {
-			return false;
-		}
 	}
 }
